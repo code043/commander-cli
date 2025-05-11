b1eeef970490fe9aadbd3e7d99f991d6a9db9503
code043 <procoder043@gmail.com>
2025-05-11 19:27:09 -0300

    Add test

---
+++ index.js
@@ -0,0 +1,11 @@
+const program = require('commander')
+const pkg = require('./package.json')
+
+program.version(pkg.version)
+program
+  .command('add [todo]')
+  .description('Add a task')
+  .action((todo) => {
+    console.log(todo);
+  })
+program.parse(process.argv)
