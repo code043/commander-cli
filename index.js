d33ef05974c4791c78fe2732b6a71b858fa4a7a1
code043 <procoder043@gmail.com>
2025-05-11 19:45:44 -0300

    Adding the todo

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 #!/usr/bin/env node
 const program = require('commander')
+const { join } = require('path')
+const fs = require('fs')
+const inquirer = require('inquirer')
 const pkg = require('./package.json')
+const tdsPath = join(__dirname, 'todo.json')
+
+const getJsn = (path) => {
+  const data = fs.existsSync(path) ? fs.readFileSync(path) : []
+  try {
+    return JSON.parse(data)
+  } catch (error) {
+    return []
+  }
+}
+
+const saveJsn = (path, data) => fs.writeFileSync(path, JSON.stringify(data, null, 2))
 
 program.version(pkg.version)
 program
   .command('add [todo]')
   .description('Add a task')
-  .action((todo) => {
-    console.log(todo);
+  .action(async (todo) => {
+    let anwers;
+    if(!todo){
+      anwers = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'todo',
+          message: 'What\'s your wish?'
+        }
+      ])
+    }
+    const data = getJsn(tdsPath)
+    data.push({
+      title: todo || anwers.todo,
+      done: false
+    })    
+    saveJsn(tdsPath, data)
   })
 program.parse(process.argv)
