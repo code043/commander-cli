fac9197a4d756a01fa3ab09b02196675a1813e9e
code043 <procoder043@gmail.com>
2025-05-11 22:01:45 -0300

    Do and undo tasks

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const chalk = require('chalk')
 const Table = require('cli-table')
 
 const pkg = require('./package.json')
+const { type } = require('os')
 const tdsPath = join(__dirname, 'todo.json')
 
 const getJsn = (path) => {
@@ -56,6 +57,54 @@ program
     saveJsn(tdsPath, data)
     console.log(`${chalk.green('Task has been added successfully!')}`)
   })
+program
+  .command('do <todo>')
+  .description('Mark a task as done')
+  .action(async (todo) => {
+    let anwers;
+    if(!todo){
+      anwers = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'todo',
+          message: 'What\'s id of task?',
+          validate: value => value !== undefined ? true : 'Difiny task id'
+        }
+      ])
+          
+    }
+    const data = getJsn(tdsPath)
+    data[todo].done = true;
+    saveJsn(tdsPath, data)
+    console.log(`${chalk.green('Task has been marked as done!')}`)
+    showTaskTable(data)
+  })
+program
+  .command('undo <todo>')
+  .description('Mark a task as undone')
+  .action(async (todo) => {
+    let anwers;
+    if(!todo){
+      anwers = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'todo',
+          message: 'What\s task id',
+          validate: value => value ? true : 'Difiny task id'
+
+        }
+      ])
+    }
+    const data = getJsn(tdsPath)
+    data[todo].done = false;
+    saveJsn(tdsPath, data)
+    console.log(`${chalk.green('Task has been marked as undone!')}`)
+    showTaskTable(data)
+
+  })
+
+
+
 program
   .command('list') 
   .description('List all tasks')
